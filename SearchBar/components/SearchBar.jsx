fc24513101e5859ac40fbde7f2ad809095464a19
Herman Rademeyer <98677997+maans-dev@users.noreply.github.com>
2022-08-05 00:36:00 +0200

    Update SearchBar.jsx

--- SearchBar/components/SearchBar.jsx
+++ SearchBar/components/SearchBar.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './SearchBar.css'
 
 const SearchBar = ( {placehoder, data}) => {
+    const [filter, setFilter] = useState([]);
+
+    const handleFilter = (event) => {
+        const word = event.target.value
+        const newFilter = data.filter((value)=>{
+            return value.title.toLowerCase.includes(word.toLowerCase)
+        })
+    setFilter(newFilter);
+    }
+
   return (
     <div className='searchBar'>
         <div className='seatchInput'>
-            <input type="text" />
+            <input type="text" placeholder={placehoder} onChange={handleFilter}/>
         </div>
+        {filter.length != 0 && (
         <div className='dataResult'>
-            {data.map((value, key) => {
+            {setFilter.map((value, key) => {
                 return (
                  <a className='dataItem' href={value.link} >
                    <p> {value.title} </p>
                  </a> )
             })}
         </div>
+)}
     </div>
   )
 }
