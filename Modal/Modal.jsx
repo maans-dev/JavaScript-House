fc73371369ae00b8e1e042a640b0250cfdc2c52a
Herman Rademeyer <98677997+maans-dev@users.noreply.github.com>
2022-08-04 23:31:56 +0200

    Create Modal.jsx

---
+++ Modal/Modal.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Modal.css'
+
+const Modal = ({props}) => {
+  return (
+    <div className='modalBackground'>
+        <div className='modalContainer'>
+            <div className='titleCloseBtn'>
+            <button onClick={()=>{props(false)}}> Cancel </button>
+            </div>
+            <div className='title'>
+                <h1>Continue or Go Back</h1>
+            </div>
+            <div className='body'>
+                Click to the next page
+            </div>
+            <div className='footer'>
+                <button>Continue</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default M
